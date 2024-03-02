d6198edd416cbb93c6185dd2728a141ba6d4c24f
dangvu2405 <142129742+dangvu2405@users.noreply.github.com>
2024-03-02 09:46:07 +0700

    Update baitaplop.js

--- baitaplop.js
+++ baitaplop.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
+const app = require('./app')
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 // Trang chủ
 app.get('/', (req, res) => {
